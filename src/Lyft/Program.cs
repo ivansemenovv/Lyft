4c5adb4f799dda4dfee54bc8cb6049296c0505fe
Ivan Semenov <ivsemeno@microsoft.com>
2018-05-24 12:50:17 -0700

    add basic input file

--- src/Lyft/Program.cs
+++ src/Lyft/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,18 @@ namespace Lyft
     {
         static void Main(string[] args)
         {
+            // For stdin, you could use
+            // new StreamReader(Console.OpenStandardInput(), Console.InputEncoding)
 
+            using (var b = new StreamReader("InputFile.txt"))
+            {
+                string line;
+                while ((line = b.ReadLine()) != null)
+                    Console.WriteLine(line);
+            }
+
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
         }
     }
 }
