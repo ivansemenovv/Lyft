3539cf8f39c031586d19468397402b0c0d1df143
Ivan Semenov <ivsemeno@microsoft.com>
2018-05-25 13:50:42 -0700

    1 version

--- src/Lyft/Program.cs
+++ src/Lyft/Program.cs
@@ -11,18 +11,57 @@ namespace Lyft
     {
         static void Main(string[] args)
         {
-            // For stdin, you could use
-            // new StreamReader(Console.OpenStandardInput(), Console.InputEncoding)
-
-            using (var b = new StreamReader("InputFile.txt"))
+            ValueStore store = new ValueStore();
+            string line = string.Empty;
+            string key = string.Empty;
+            while((line = Console.ReadLine()) != "q")
             {
-                string line;
-                while ((line = b.ReadLine()) != null)
-                    Console.WriteLine(line);
-            }
+                var parts = line.Split();
+                switch(parts[0])
+                {
+                    case "PUT":
+                        key = parts[1];
+                        var value = Int32.Parse(parts[2]);
+                        store.Put(key, value);
+                        Console.WriteLine("PUT(#{0}) {1} = {2}", store.Version, key, value);
+                        break;
+                    case "GET":
+                        key = parts[1];
+                        int version = -1;
+                        if(parts.Length > 2)
+                        {
+                            version = Int32.Parse(parts[2]);
+                        }
 
-            Console.WriteLine("Press enter to exit");
-            Console.ReadLine();
+                        var v = store.Get(key, version);
+                        if(v.HasValue)
+                        {
+                            if (version == -1)
+                            {
+                                Console.WriteLine("GET {0} = {1}", key, v);
+                            }
+                            else
+                            {
+                                Console.WriteLine("GET {0}(#{1}) = {2}", key, version, v);
+                            }
+                        }
+                        else
+                        {
+                            if (version == -1)
+                            {
+                                Console.WriteLine("GET {0} = <NULL>", key);
+                            }
+                            else
+                            {
+                                Console.WriteLine("GET {0}(#{1}) = <NULL>", key, version);
+                            }
+                        }
+
+                        break;
+                    default: break;
+                }
+            }
+            
         }
     }
 }
---
+++ src/Lyft/ValueStore.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lyft
+{
+    public class VersionedValue
+    {
+        public int Value
+        {
+            get;
+        }
+        public int Version
+        {
+            get;
+        }
+        public VersionedValue(int value, int version)
+        {
+            Value = value;
+            Version = version;
+        }
+    }
+
+    public class ValueStore
+    {
+        private Dictionary<string, List<VersionedValue>> store = new Dictionary<string, List<VersionedValue>>();
+
+        public ValueStore()
+        {
+            Version = 0;
+        }
+
+        public int Version
+        {
+            get;
+            private set;
+        }
+
+        public void Put(string key, int value)
+        {
+            Version++;
+            if (store.ContainsKey(key))
+            {
+                store[key].Add(new VersionedValue(value, Version));
+            }
+            else
+            {
+                store.Add(key, new List<VersionedValue>() { new VersionedValue(value, Version) });
+            }
+        }
+
+        public int? Get(string key, int version = -1)
+        {
+            if (store.ContainsKey(key))
+            {
+                if (version == -1)
+                {
+                    int len = store[key].Count;
+                    return store[key][len - 1].Value;
+                }
+                else
+                {
+                    var list = store[key];
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if(list[i].Version > version && i > 0)
+                        {
+                            return list[i - 1].Value;
+                        }
+                    }
+
+                    int len = store[key].Count;
+                    return store[key][len - 1].Value;
+                }
+            }
+            return null;
+        }
+    }
+}
